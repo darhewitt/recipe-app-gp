f65d4de0a7b06af70cb0848cc61a4edc63191dbc
Ubuntu <ubuntu@ip-172-31-81-42.ec2.internal>
2019-11-05 22:22:24 +0000

    connecting to DB

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipes_app'
+app.config["MONGO_URI"] = 'mongodb://dhewitt:Apple1991@myfirstcluster-shard-00-00-t0occ.mongodb.net:27017,myfirstcluster-shard-00-01-t0occ.mongodb.net:27017,myfirstcluster-shard-00-02-t0occ.mongodb.net:27017/recipes_app?ssl=true&replicaSet=myFirstCluster-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_tasks():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
